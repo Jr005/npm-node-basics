9ff7d517a44e3374b0a225444b3fc32aa83dd7b5
rubenjr005 <rubenjr005@gmail.com>
2020-09-24 00:16:05 -0500

    I had to make changes

--- Express.js
+++ Express.js
@@ -18,6 +18,14 @@ app.get("/gallery", (req, res) => {
   res.send("<h1>HI Gallery</h1>");
 });
 
+app.get("/about", (req, res) => {
+  res.send("<h1>About</h1>");
+});
+
+app.get("/gallery", (req, res) => {
+  res.send("<h1>HI Gallery</h1>");
+});
+
 app.listen(3000, () => {
   console.log(
     "Server is running on http://www.localhost:3000/ to stop server press control+c"
